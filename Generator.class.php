360766ccd9c043019bc07f10d41d20c00390f0b5
makinin-dotcom <judah.makinin@amalitech.org>
2021-04-25 11:19:23 +0000

    unique ID generator

---
+++ Generator.class.php
@@ -0,0 +1,55 @@
+<?php
+
+require './Database/Database.class.php';
+
+class Generate{
+    public $conn;
+    /**
+     * 
+     * @param $table_name String - the table to use
+     * 
+     * @param $prefix_ String - the prefix settings of the IDs to generate
+     * 
+     * @param $column_name String - the name of the column to fetch the data from
+     * 
+     */
+
+    function __construct(){
+        $this->conn = new Database();
+
+        
+    }
+
+    function generate ( $table_name, $prefix_, $column_name ){
+
+
+        $sql = "SELECT COUNT('$column_name') as mk FROM $table_name";
+        
+        $execute_query  =  $this->conn->query($sql);
+
+        $result = $execute_query->fetcharray(SQLITE3_ASSOC);
+
+        $last_value = $result['mk'];
+
+
+        /*
+            typecast |$last_value|, add 1 and prepend with $prefix_
+        */
+            if (is_null($last_value)) {
+                $new_id = 1;
+            }
+            else{
+                $new_id = (int)$last_value + 1;
+            }
+
+        $new_id = $prefix_.(str_pad(($new_id), 5, "0", STR_PAD_LEFT));
+
+    return $new_id;	
+    }
+}
+
+
+$instantiate = new Generate();
+
+
+echo $instantiate->generate('members_data', 'OFBCM', 'member_id');
--- README.md
+++ README.md
@@ -1,7 +1,16 @@
 # unique-id-generator
-Generate unique ID numbers for database entries based on already existing records
+Generate unique ID numbers for database entries 
+
+It can be based on already existing records or
+It can start with an empty database table
+
+### PHP Based
 
 The system is PHP coded
-Create a database and use the connection string in the models file to connect to it
-Set the right table and column
-Access and retrieve the data and generate the right unique ID number
+- Create an SQLite3 database (more information at https://sqlite.org/index.html )
+- Use the connection setting in the Database class file to connect to it
+- Set the right table and column parameters
+- Access and retrieve the data and generate the right unique ID number
+
+
+- Thank You and Enjoy 💞💗
